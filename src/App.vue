02f99e1513bdd3195601f64153f16d31e325ff10
Dmitry <dime10cents@gmail.com>
2017-09-20 11:08:19 +0600

    add styles

--- src/App.vue
+++ src/App.vue
@@ -23,30 +23,65 @@
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-a {
-  color: #42b983;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    min-height: 90px;
+    border-bottom: 1px solid #42b983;
+    text-align: center;
+    background: #ffffff;
+  }
+
+  main {
+    display: flex;
+    height: calc(100vh - 90px);
+    max-width: 1200px;
+    margin-top: 90px;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: hidden;
+  }
+
+  aside {
+    flex: 1 0 30%;
+    height: 100%;
+    overflow-y: auto;
+    width: 30%;
+    padding: 50px 30px;
+    box-sizing: border-box;
+    border-right: 1px solid #42b983;
+  }
+  .content {
+    flex: 1 1 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
