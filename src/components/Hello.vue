a116b7cc0b7c7904d9046d673071050009f1b128
Dmitry <dime10cents@gmail.com>
2017-09-20 11:54:21 +0600

    step 4

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,3 +1,18 @@
-<template>
-  <h2>Hello</h2>
+<template lang="html">
+  <div class="greeting">
+    <h2>
+      Wellcome to the Vue.js blog
+    </h2>
+    <img src="../assets/logo.png" alt="vuejs logo">
+  </div>
 </template>
+
+<style lang="scss">
+  .greeting {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+  }
+</style>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,3 +1,74 @@
-<template>
-  <h2>Post</h2>
+<template lang="html">
+  <div class="post" v-if="post">
+    <h1 class="post__title">{{ post.title }}</h1>
+    <p class="post__body">{{ post.body }}</p>
+    <p  class="post__id">{{ post.id }}</p>
+  </div>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    props: ['id'],
+
+    data() {
+      return {
+        post: null,
+        endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+      }
+    },
+
+    methods: {
+      getPost(id) {
+        axios(this.endpoint + id)
+          .then(response => {
+            this.post = response.data
+          })
+          .catch( error => {
+            console.log('-----error-------');
+            console.log(error)
+          })
+      }
+    },
+    
+    created() {
+      this.getPost(this.id);
+    },
+
+    watch: {
+      '$route'() {
+        this.getPost(this.id);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    position: relative;
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 50px 20px 70px;
+    &__title {
+      position: relative;
+      text-transform: uppercase;
+      z-index: 1;
+    }
+    &__body {
+      position: relative;
+      z-index: 1;
+    }
+    &__id {
+      position: absolute;
+      font-size: 280px;
+      bottom: -50px;
+      margin: 0;
+      color: #eeeeee;
+      right: -20px;
+      line-height: 1;
+      font-weight: 900;
+      z-index: 0;
+    }
+  }
+</style>
